c3028516999f26689923d88a3f55b03079b558ee
Carlos Alexandre <alexfilho062005@gmail.com>
2024-09-11 13:19:16 -0300

    Caminho das imagens

--- src/pages/home/components/procedures/utils/data.tsx
+++ src/pages/home/components/procedures/utils/data.tsx
@@ -1,52 +1,61 @@
 import { CardDTO } from '@/models/CardDTO';
 import { BASE_MESSAGE_WPP } from '@/utils/system';
 
+import clareamentoImg from '@/assets/procedures/clareamento.jpg';
+import botoxImg from '@/assets/procedures/botox.jpg';
+import detoxImg from '@/assets/procedures/detox.jpg';
+import estriasImg from '@/assets/procedures/estrias.jpg';
+import seborreiaImg from '@/assets/procedures/seborreia.jpg';
+import calvicieImg from '@/assets/procedures/calvicie.jpg';
+import peelingImg from '@/assets/procedures/peeling.jpg';
+import preenchimentoImg from '@/assets/procedures/preenchimento.jpg';
+
 export const procedimentos: CardDTO[] = [
   {
     id: 1,
-    bg: 'src/assets/procedures/clareamento.jpg',
+    bg: clareamentoImg,
     title: 'CLAREAMENTO',
     message: `${BASE_MESSAGE_WPP}clareamento.`,
   },
   {
     id: 2,
-    bg: 'src/assets/procedures/botox.jpg',
+    bg: botoxImg,
     title: 'BOTOX',
     message: `${BASE_MESSAGE_WPP}Botox.`,
   },
   {
     id: 3,
-    bg: 'src/assets/procedures/detox.jpg',
+    bg: detoxImg,
     title: 'DETOX',
     message: `${BASE_MESSAGE_WPP}Detox`,
   },
   {
     id: 4,
-    bg: 'src/assets/procedures/estrias.jpg',
+    bg: estriasImg,
     title: 'ESTRIAS',
     message: `${BASE_MESSAGE_WPP}estrias`,
   },
   {
     id: 5,
-    bg: 'src/assets/procedures/seborreia.jpg',
+    bg: seborreiaImg,
     title: 'SEBORREIA',
     message: `${BASE_MESSAGE_WPP}seborreia`,
   },
   {
     id: 6,
-    bg: 'src/assets/procedures/calvicie.jpg',
+    bg: calvicieImg,
     title: 'CALVÍCIE',
     message: `${BASE_MESSAGE_WPP}calvície`,
   },
   {
     id: 7,
-    bg: 'src/assets/procedures/peeling.jpg',
+    bg: peelingImg,
     title: 'PEELING',
     message: `${BASE_MESSAGE_WPP}Peeling`,
   },
   {
     id: 8,
-    bg: 'src/assets/procedures/preenchimento.jpg',
+    bg: preenchimentoImg,
     title: 'PREENCHIMENTO',
     message: `${BASE_MESSAGE_WPP}preenchimento`,
   },
